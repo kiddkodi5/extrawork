dc5ada52e6076cd3cb160a0b30852e788e1593ef
千葉広大 <chibakoudai@chibakoudai-no-MacBook-Pro.local>
2020-09-18 22:13:12 +0900

    9/18 検索機能大枠完成

--- source code/list.php
+++ source code/list.php
@@ -29,6 +29,45 @@
 	
 	<h3>アカウント一覧画面</h3>
 	
+	<form action=""  method="post">
+	<table border="1px">
+		<tr>
+			<td>名前（姓）</td>
+			<td><input type="text" name="family_name" value="<?php if(isset($_POST['family_name'])){echo $_POST['family_name'];} ?>"></td>
+			<td>名前（名）</td>
+			<td><input type="text" name="first_name" value="<?php if(isset($_POST['first_name'])){echo $_POST['first_name'];} ?>"></td>
+		</tr>
+		<tr>
+			<td>カナ（姓）</td>
+			<td><input type="text" name="family_name_kana" value="<?php if(isset($_POST['family_name_kana'])){echo $_POST['family_name_kana'];} ?>"></td>
+			<td>カナ（名）</td>
+			<td><input type="text" name="first_name_kana" value="<?php if(isset($_POST['first_name_kana'])){echo $_POST['first_name_kana'];} ?>"></td>
+		</tr>
+		<tr>
+			<td>メールアドレス</td>
+			<td><input type="text" name="mail" value="<?php if(isset($_POST['mail'])){echo $_POST['mail'];} ?>"></td>
+			<td>性別</td>
+			<td><input type="radio" name="gender" value="0">男
+				<input type="radio" name="gender" value="1">女
+				<input type="radio" name="gender" value="2" checked>なし
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<select class="dropdown" name="authority">		
+					<option value="0">一般</option>
+					<option value="1">管理者</option>
+					<option value="2" selected>なし</option>
+				</select>
+			</td>
+			<td>
+		<input type="submit" class="submit" value="検索">
+			</td>
+		</tr>
+		</table>
+		
+	</form>
+	
 	
 	<?php
 			mb_internal_encoding("utf-8");
@@ -36,23 +75,61 @@
 			try{
 				$pdo = new PDO("mysql:dbname=assighment;host=localhost;","root","root");
 			/*echo '接続成功';*/
-			
 				
-			$stmt = $pdo->query("select * from account order by id DESC");
-			
+	if(isset($_POST['family_name'])){
+		$family_name = $_POST['family_name'];
+		$first_name = $_POST['first_name'];
+		$family_name_kana = $_POST['family_name_kana'];
+		$first_name_kana = $_POST['first_name_kana'];
+		$mail = $_POST['mail'];
+		$gender = $_POST['gender'];
+		$authority = $_POST['authority'];
+					
+				
+				
+		/*	↓全部の値が空でなおかつ性別＆権限が２（なし）の時＝そのまま検索ボタン押下*/
+			if(empty($_POST['family_name']) and empty($_POST['first_name']) and empty($_POST['family_name_kana']) and empty($_POST['first_name_kana']) and empty($_POST['mail']) and $_POST['gender'] =="2" and $_POST['authority'] == "2"){
+				$stmt = $pdo->query("select * from account order by id DESC");
+				echo "2";
+				/*↓どっかに何かしらの値が入っていたら検索ヒットした項目のみ表示*/
+			}elseif(isset($_POST['family_name']) or isset($_POST['first_name']) or isset($_POST['family_name_kana']) or isset($_POST['first_name_kana']) or isset($_POST['mail']) or isset($_POST['gender']) or isset($_POST['authority'])){
+				echo "9";
+				$sql = "select * from account ";
+				$cnt = 0;
+				if(isset($_POST['family_name'])){
+					if($cnt == 0){
+						$sql = $sql."where ";
+					}else{
+						$sql = $sql."and ";
+					}
+					$sql = $sql."family_name LIKE '%$family_name%' ";
+					$cnt++;
+				}
+				if(isset($_POST['first_name'])){
+					$sql = "where family_name LIKE '%$first_name%'";
+				}
+				if(isset($_POST['family_name_kana'])){
+					$sql = "where family_name LIKE '%$family_name_kana%'";
+				}
+				if(isset($_POST['first_name_kana'])){
+					$sql = "where family_name LIKE '%$first_name_kana%'";
+				}
+				if(isset($_POST['mail'])){
+					$sql = "where family_name LIKE '%$mail%'";
+				}
+				
+				
+				/*$sql = "select * from account where family_name LIKE '%$family_name%' and first_name LIKE '%$first_name%' and family_name_kana LIKE '%$first_name_kana%' and mail LIKE '%$mail%'and gender = 1 order by id DESC";*/
+				
+				echo $family_name;
+				$stmt = $pdo->query($sql."order by id DESC");
+			}else{echo "何もなし";}
+				
 			while($row = $stmt->fetch()){
 				$rows[] =$row;
 			}
-			
-			/*　↑　=foreach($stmt as $row){
-				$rows[] = $row;
-			}*/
-			
-			$pdo= null;
-			/*データベース切断*/
-				
-	?>
-			<table border="1px">
+			?>		
+		<table border="1px">
 	<tr>
 		<td>ID</td>
 		<td>名前（姓）</td>
@@ -67,7 +144,15 @@
 		<td>更新日時</td>
 		<td>操作</td>
 	</tr>
-			<?php foreach($rows as $row){
+			<?php
+				}
+			$pdo= null;
+			/*データベース切断*/
+			
+	?>
+	
+	
+			<?php if(isset($_POST['family_name'])){foreach($rows as $row){
 	?>
 	<tr>
 		<td><?php echo $row['id']; ?></td>
@@ -130,7 +215,7 @@
 			</form>
 				</td>
 	</tr>
-<?php } 
+<?php } }
 		
 				
 			}catch(PDOException $e){
